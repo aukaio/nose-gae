e4a26619c5c03ab4940dade553a5b8ff7ddbe48d
Helge Tesdal <helge@tesdal.com>
2012-05-22 23:54:37 +0200

    CORE-194 Added NoseGAE 0.2.1 (dev) as develop egg to try to get the gae environ correct and in turn get correct webob library

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+DESCRIPTION = """\
+NoseGAE: nose plugin for Google App Engine testing\
+"""
+LONG_DESCRIPTION = """\
+Basic usage::
+    
+    $ cd your/app
+    $ nosetests --with-gae
+
+See http://farmdev.com/projects/nosegae/ for complete documentation or open docs/readme.html after downloading the package.
+
+"""
+VERSION = '0.2.1'
+
+setup(
+    name='NoseGAE',
+    version=VERSION,
+    author="Jason Pellerin",
+    author_email="jpellerin@gmail.com",
+    maintainer="Kumar McMillan",
+    maintainer_email="kumar.mcmillan@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        ("License :: OSI Approved :: GNU Library or "
+         "Lesser General Public License (LGPL)"),
+        "Topic :: Software Development :: Testing"
+        ],
+    url='http://code.google.com/p/nose-gae/',
+    license='LGPL License',
+    entry_points = {
+        'nose.plugins.0.10': [ 'nosegae = nosegae:NoseGAE']
+        },
+    py_modules = ['nosegae'],
+    install_requires = ['nose>=0.10.1'],
+    tests_require = ['WebTest', 'trestle>=0.2a1']
+    )
