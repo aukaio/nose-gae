64f356b6b85de1b3e7352e76eca966fe06754f8a
espensb <espensb@nt.ntnu.no>
2013-06-20 16:30:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Basic usage::
 See http://farmdev.com/projects/nosegae/ for complete documentation or open docs/readme.html after downloading the package.
 
 """
-VERSION = '0.2.1-mcash'
+VERSION = '0.2.1mcash'
 
 setup(
     name='NoseGAE',
